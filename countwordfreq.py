2f0d7079f5deb622bea49f92ac7539e83e62eb0d
Jon Barker <jonathan.t.barker@gmail.com>
2014-02-09 10:24:46 -0500

    first commit

---
+++ countwordfreq.py
@@ -0,0 +1,43 @@
+#! /usr/bin/python
+""" this program breaks a string into a list of component words 
+and counts each word, by Jon Barker, 2014-1-7"""
+# import matplotlib
+# import os 
+# print(dir(matplotlib))
+def main():
+    print "input a sentence and i will count the words for you"
+    sentence = str(input("your sentence:  "))
+main()
+# def wordcount(x):
+#     global a
+#     a = x.split(" ")
+
+
+# if __name__ == "__main__":
+#     main()
+# tester = "i am a an awesome person hello thanks hello goodbye"
+
+
+# def wordcount(x):
+#     global a 
+#     a = x.split(" ")
+#     return a
+	
+
+# wordcount(tester)
+
+# lis = []
+
+
+# for i in range(1,len(a)):
+#     if j in a == i:
+
+
+
+
+
+
+# for c in tester:
+# 	lis.append(c)
+
+
